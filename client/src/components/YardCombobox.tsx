7c57dd570e22174d17cb9d837ae282a2cb19d5bf
Otavio Miklos <otaviomiklos@gmail.com>
2025-09-15 00:12:00 -0300

    chore: only yards from selected subsidiary appear

--- client/src/components/YardCombobox.tsx
+++ client/src/components/YardCombobox.tsx
@@ -16,54 +16,25 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-import { Yard } from "@/lib/types";
-import axios from "axios";
-import { NEXT_PUBLIC_JAVA_URL } from "@/lib/environment";
 import { MapPin } from "lucide-react";
-import { Notification } from "./Notification";
-import { clearNotification } from "@/lib/utils";
 import { useSnapshot } from "valtio";
-import { areaCreationStore } from "@/lib/valtio";
+import { areaCreationStore, subsidiaryStore } from "@/lib/valtio";
+import { Yard } from "@/lib/types";
 
 export function YardCombobox() {
+  const snapSubsidiary = useSnapshot(subsidiaryStore);
   const snapAreaCreation = useSnapshot(areaCreationStore);
 
   const [open, setOpen] = React.useState(false);
 
-  const [yards, setYards] = React.useState<Yard[]>([]);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const [notification, setNotification] = React.useState<string | undefined>(
-    undefined
-  );
-
-  React.useEffect(() => {
-    async function fetchYards() {
-      setLoading(true);
-
-      try {
-        const response = await axios.get(`${NEXT_PUBLIC_JAVA_URL}/yards`);
-        setYards(response.data);
-      } catch {
-        setNotification("Não foi possível buscar os pátios");
-      } finally {
-        clearNotification<string | undefined>(setNotification, undefined);
-        setLoading(false);
-      }
-    }
-
-    fetchYards();
-  }, []);
-
   return (
     <div className="flex items-center space-x-4 relative">
-      {notification && <Notification title="Ops!" message={notification} />}
 
       <p className="text-muted-foreground text-sm">Pátio</p>
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
             variant="outline"
-            disabled={loading}
             className="justify-start"
           >
             {snapAreaCreation.yard ? (
@@ -83,15 +54,15 @@ export function YardCombobox() {
             <CommandList>
               <CommandEmpty>Nenhum resultado encontrado.</CommandEmpty>
               <CommandGroup>
-                {yards.map((yard) => (
+                {snapSubsidiary.subsidiary?.yards.map((yard) => (
                   <CommandItem
                     key={yard.id}
                     value={yard.name.toString()}
                     onSelect={(value) => {
                       areaCreationStore.yard =
-                        yards.find(
+                        snapSubsidiary.subsidiary?.yards.find(
                           (priority) => priority.name.toString() == value
-                        ) || undefined;
+                        ) as Yard || undefined;
                       setOpen(false);
                     }}
                   >
